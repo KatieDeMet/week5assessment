1b82070300fe92e2761f822b4be13df891620009
Katie DeMet <katie.demet7@gmail.com>
2021-08-06 11:21:22 -0500

    partial assignment

--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ app.use(cors());
 
 app.use(express.json()); // When we want to be able to accept JSON.
 
+let goals = ["Make it work"];
+
 app.get("/api/compliment", (req, res) => {
   const compliments = ["Gee, you're a smart cookie!",
 					 "Cool shirt!",
@@ -21,5 +23,42 @@ app.get("/api/compliment", (req, res) => {
   res.status(200).send(randomCompliment);
   
 });
+
+app.get("/api/fortune", (req, res) => {
+  const fortunes = ["Career change coming soon",
+    "New romance on the horizon", "Windfall headed your way", 
+    "Storm clouds up ahead", "New baby coming soon", "Don't drink the Kool-aid"]
+
+    let randomIndex = Math.floor(Math.random() * fortunes.length);
+    let randomFortune = fortunes[randomIndex];
+
+    res.status(200).send(randomFortune);
+});
+
+app.get("/api/words", (req, res) => {
+  const positiveWords = ["Happy!", "Healthy!", "Beautiful!", "Wonderful!", "Smart!", "Joyful!", "Peaceful!", "Lucky!"];
+
+  let randomIndex = Math.floor(Math.random() * positiveWords.length);
+  let randomWord = positiveWords[randomIndex];
+
+  res.status(200).send(randomWord);
+  
+});
+
+app.post("/api/goals", (req, res) => {
+  let {goal} = req.body;
+  goals.push(goal);
+  res.status(200).send(goals);
+})
+
+app.get("/api/goals", (req, res) => {
+  res.status(200).send(goals);
+})
+
+app.get("/api/quotes", (req, res) => {
+  let quotes = ["The best preparation for tomorrow is doing your best today.",
+    "Keep your face always toward the sunshine and shadows will fall behind you."]
+  res.status(200).send();
+})
 
 app.listen(4000, () => console.log("Server running on 4000"));
